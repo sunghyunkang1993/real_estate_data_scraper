a63cecb7b0b7d24349bc7d02cd02d8e6b8644f21
garg-anubhav53 <148268431+garg-anubhav53@users.noreply.github.com>
2023-10-25 23:10:55 -0500

    Update spider.py to store items as a dictionary and to find messages within post
    Updated based on working session and additional work to do: 
    --Store of webscraping in a dictionary
    --Finding content from paragraphs within a post
    --Attempts at next steps explained

--- real_estate_data_scraper/spiders/spider.py
+++ real_estate_data_scraper/spiders/spider.py
@@ -1,27 +1,77 @@
 import scrapy
 from scrapy.http.response import Response
 
+
 class BPSpider(scrapy.Spider):
-    name = "bp"
+  name = "bp"
+  print("This statement runs in the BPSpider constructor")
 
-    def start_requests(self):
-        urls = [
-            "https://www.biggerpockets.com/forums?page=1"
+  def start_requests(self):
+    urls = ["https://www.biggerpockets.com/forums?page=1"]
+
+    # We want to be able to search and scrape for specific search terms like theses
+    # urls = [
+    #     "https://www.biggerpockets.com/search/topics?term=indianapolis+cash+flow",
+    #     "https://www.biggerpockets.com/search/topics?term=interest+rate+adjustments"
+    # ]
+    for url in urls:
+      yield scrapy.Request(url=url, callback=self.parse)
+
+  def parse(self, response: Response):
+    print("This statement runs at the beginning of the parse function")
+
+    PostDict = {
+        0: [
+            "ExampleTitle", "ExampleURL", "ExampleAuthor", "ExampleTopic",
+            "ExampleProfLink", "ExampleUserProfLink", "ExamplePostURLLink"
         ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    }
+    # title, url, author, topic, profile_link, user_profile_link, post_url_link,
 
-    def parse(self, response: Response):
-        posts = response.xpath('//div[contains(@class, "simplified-forums__card-wrapper-compact")]')
-        for post in posts:
-            post_title = post.xpath('.//div[contains(@class, "simplified-forums__card-header-compact")]//a/text()').get()
-            author = post.xpath('.//a[contains(@class, "simplified-forums__user__profile-link")]/text()').get()
-            topic = post.xpath('.//a[contains(@class, "simplified-forums__topic-metadata__link")]/text()').get()
-            profile_link = post.xpath('.//a[contains(@class, "simplified-forums__user__profile-link")]//@href').get()
-            user_profile_link = "https://www.biggerpockets.com" + profile_link
-            yield scrapy.Request(user_profile_link, callback=self.parse_user)
+    posts = response.xpath(
+        '//div[contains(@class, "simplified-forums__card-wrapper-compact")]')
+    i = 0
+    for post in posts:
+      i += 1
+      post_title = post.xpath(
+          './/div[contains(@class, "simplified-forums__card-header-compact")]//a/text()'
+      ).get()
+      post_url = post.xpath(
+          './/h2[contains(@class, "simplified-forums__topic-content__title")]//a//@href'
+      ).get()
+      author = post.xpath(
+          './/a[contains(@class, "simplified-forums__user__profile-link")]/text()'
+      ).get()
+      topic = post.xpath(
+          './/a[contains(@class, "simplified-forums__topic-metadata__link")]/text()'
+      ).get()
+      profile_link = post.xpath(
+          './/a[contains(@class, "simplified-forums__user__profile-link")]//@href'
+      ).get()
+
+      print("This is the post URL if it was retrieved: " + post_url)
+      user_profile_link = "https://www.biggerpockets.com" + profile_link
+      post_url_link = "https://www.biggerpockets.com" + post_url
+      yield scrapy.Request(post_url_link, callback=self.parse_post)
+
+      PostDict[i] = [
+          post_title, post_url, author, topic, profile_link, user_profile_link,
+          post_url_link
+      ]
+      print(post_title, author, topic, profile_link, user_profile_link)
+
+    print("Here's the dict of posts: ", PostDict)
+
+  def parse_post(self, response: Response):
+    ### 403 ERROR for SCRAPE
+    print(
+        response.xpath(
+            '//div[contains(@class, "simplified-forums__topic-content__body-container")]//p'
+        ).getall())
 
 
-    def parse_user(self, response: Response):
-        ### 403 ERROR for SCRAPE
-        print(response.xpath('//div[contains(@class, "user-about-me")]//p').getall())
+# BPScrapeInstance = BPSpider(name="bp")
+# BPScrapeInstance.start_requests()
+# BPScrapeInstance.parse(BPScrapeInstance.start_requests())
+
+print("This statement prints after instance runs")
