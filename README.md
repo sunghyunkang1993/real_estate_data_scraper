09666ec6de7061dcfe7ec89aa7b28a6ff0e7219a
Sung-Hyun Kang <sunghyunkang@microsoft.com>
2023-10-26 18:31:52 -0500

    save the data structure in file

--- README.md
+++ README.md
@@ -6,8 +6,9 @@
 3. For each post on the forum, it will find post title, author, and topic. Also it will find the user name of the author.
 4. it will try to craw the author's profile page
 
-## Moifications
-1. Crawl the forum page and find the href link
-2. Crawl the actual post page that has comments from the href from 1
+## How to run the scraper
+`scrapy crawl bp -o bp.json`
+`scrapy crawl bp -o bp.csv`
+This will save scraped data in the datastructure that we have determined in the json file `bp.json` or `bp.csv` depending on the chosen format
 
 
--- real_estate_data_scraper/spiders/spider.py
+++ real_estate_data_scraper/spiders/spider.py
@@ -1,38 +1,26 @@
 import scrapy
 from scrapy.http.response import Response
+import json
 
 
 class BPSpider(scrapy.Spider):
   name = "bp"
   print("This statement runs in the BPSpider constructor")
 
-  def start_requests(self):
-    urls = ["https://www.biggerpockets.com/forums?page=1"]
+  def __init__(self):
+    self.post_data = []
 
-    # We want to be able to search and scrape for specific search terms like theses
-    # urls = [
-    #     "https://www.biggerpockets.com/search/topics?term=indianapolis+cash+flow",
-    #     "https://www.biggerpockets.com/search/topics?term=interest+rate+adjustments"
-    # ]
+  def start_requests(self):
+    urls = ["https://www.biggerpockets.com/forums?page=1", "https://www.biggerpockets.com/forums?page=2", "https://www.biggerpockets.com/forums?page=3", "https://www.biggerpockets.com/forums?page=4", "https://www.biggerpockets.com/forums?page=5"]
     for url in urls:
       yield scrapy.Request(url=url, callback=self.parse)
 
   def parse(self, response: Response):
-    print("This statement runs at the beginning of the parse function")
-
-    PostDict = {
-        0: [
-            "ExampleTitle", "ExampleURL", "ExampleAuthor", "ExampleTopic",
-            "ExampleProfLink", "ExampleUserProfLink", "ExamplePostURLLink"
-        ]
-    }
-    # title, url, author, topic, profile_link, user_profile_link, post_url_link,
-
     posts = response.xpath(
         '//div[contains(@class, "simplified-forums__card-wrapper-compact")]')
-    i = 0
-    for post in posts:
-      i += 1
+
+    for index in range(len(posts)):
+      post = posts[index]
       post_title = post.xpath(
           './/div[contains(@class, "simplified-forums__card-header-compact")]//a/text()'
       ).get()
@@ -49,29 +37,22 @@ class BPSpider(scrapy.Spider):
           './/a[contains(@class, "simplified-forums__user__profile-link")]//@href'
       ).get()
 
-      print("This is the post URL if it was retrieved: " + post_url)
       user_profile_link = "https://www.biggerpockets.com" + profile_link
       post_url_link = "https://www.biggerpockets.com" + post_url
-      yield scrapy.Request(post_url_link, callback=self.parse_post)
+      
+      post_dict = {
+        "post_title": post_title,
+        "post_url": post_url_link,
+        "author": author,
+        "topic": topic,
+        "profile_link": user_profile_link
+      }
+      yield scrapy.Request(post_url_link, callback=self.parse_post, cb_kwargs=dict(post_dict=post_dict))
 
-      PostDict[i] = [
-          post_title, post_url, author, topic, profile_link, user_profile_link,
-          post_url_link
-      ]
-      print(post_title, author, topic, profile_link, user_profile_link)
-
-    print("Here's the dict of posts: ", PostDict)
-
-  def parse_post(self, response: Response):
+  def parse_post(self, response: Response, post_dict):
     ### 403 ERROR for SCRAPE
-    print(
-        response.xpath(
-            '//div[contains(@class, "simplified-forums__topic-content__body-container")]//p'
-        ).getall())
-
-
-# BPScrapeInstance = BPSpider(name="bp")
-# BPScrapeInstance.start_requests()
-# BPScrapeInstance.parse(BPScrapeInstance.start_requests())
-
-print("This statement prints after instance runs")
+    initial_post = response.xpath(
+        '//div[contains(@class, "simplified-forums__topic-content__body-container")]//p'
+    ).getall()
+    post_dict["initial_post"] = initial_post
+    yield post_dict
